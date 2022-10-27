0a4736cbe218160b945c2b5e2bdfc59c12e6308f
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2022-10-27 20:42:33 +0530

    Time: 28 ms (22.45%), Space: 20.7 MB (6.95%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,35 +1,67 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (!root)
-            return 0;
+    
+    // TC = O(N^2)
+//     int height(TreeNode* root) {
+//         if (!root)
+//             return 0;
         
-        int maxLeft = height(root->left);
-        int maxRight = height(root->right);
+//         int maxLeft = height(root->left);
+//         int maxRight = height(root->right);
         
-        return max(maxLeft, maxRight)+1;
+//         return max(maxLeft, maxRight)+1;
+//     }
+    
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         if (!root)
+//             return 0;
+        
+//         int h1 = diameterOfBinaryTree(root->left);
+//         int h2 = diameterOfBinaryTree(root->right);
+//         int h3 = height(root->left)+height(root->right);
+        
+//         int ans = max(h1, max(h2, h3));
+//         return ans;
+//     }
+    
+//     pair<int, int> diameter(TreeNode* root){
+//         // base case
+//         if(root==NULL){
+//             pair<int, int> p = make_pair(0, 0);
+//             return p;
+//         }
+        
+//         pair<int, int> left = diameter(root->left);
+//         pair<int, int> right = diameter(root->right);
+        
+//         int op1 = left.first + 1;
+//         int op2 = right.first + 1;
+//         int op3 = left.second + right.second ;
+        
+//         pair<int, int> ans;
+        
+//         ans.first =  max(op2, max(op2, op3));
+//         ans.second = max(left.second, right.second)+1;
+        
+//         return ans;
+//     }
+    
+//     int diameterOfBinaryTree(TreeNode* root){
+//         return diameter(root).first;
+//     }
+    
+     int solve(TreeNode*root,int &ans){
+        if(!root) return 0;
+        int left = solve(root->left,ans);
+        int right = solve(root->right,ans);
+        int temp = 1+max(left,right);
+        ans = max({ans,temp,1+left+right});
+        return temp;
     }
     
     int diameterOfBinaryTree(TreeNode* root) {
-        if (!root)
-            return 0;
-        
-        int h1 = diameterOfBinaryTree(root->left);
-        int h2 = diameterOfBinaryTree(root->right);
-        int h3 = height(root->left)+height(root->right);
-        
-        int ans = max(h1, max(h2, h3));
-        return ans;
+        int ans = INT_MIN;
+        solve(root,ans);
+        return ans-1;  //here they have ask for edges
     }
 };

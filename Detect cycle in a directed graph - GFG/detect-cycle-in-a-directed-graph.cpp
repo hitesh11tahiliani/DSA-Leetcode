a10e32723614484d23fa8c0e1649d8f5cd94ed13
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2022-11-22 14:26:44 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -10,41 +10,53 @@ using namespace std;
     
     // if both visted and pathvisited are marked brfore traversing the element, then cycle is present 
     
-    class Solution {
+    // dfs
+    
+//     
+
+
+class Solution {
 private:
-	bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[]) {
-		vis[node] = 1;
-		pathVis[node] = 1;
-
-		// traverse for adjacent nodes
-		for (auto it : adj[node]) {
-			// when the node is not visited
-			if (!vis[it]) {
-				if (dfsCheck(it, adj, vis, pathVis) == true)
-					return true;
-			}
-			// if the node has been previously visited
-			// but it has to be visited on the same path
-			else if (pathVis[it]) {
-				return true;
-			}
-		}
-
-		pathVis[node] = 0;
-		return false;
-	}
+	
 public:
+
+    //we will use khans algo to detect the cycle using BFS 
+    //as khans algo fail if there is a cycle present 
+    
 	// Function to detect cycle in a directed graph.
 	bool isCyclic(int V, vector<int> adj[]) {
-		int vis[V] = {0};
-		int pathVis[V] = {0};
-
-		for (int i = 0; i < V; i++) {
-			if (!vis[i]) {
-				if (dfsCheck(i, adj, vis, pathVis) == true) return true;
-			}
+		int indegree[V] = {0};
+		
+		for(int i=0; i<V; i++){
+		    for( auto it : adj[i]){
+		        indegree[it]++;
+		    }
 		}
-		return false;
+		
+		queue<int> q;
+		for(int i=0; i<V; i++){
+		    if(indegree[i]==0){
+		        q.push(i);
+		    }
+		}
+		
+		int cnt =0;
+		while (!q.empty()){
+		    int node = q.front();
+		    q.pop();
+		    cnt++;
+		  //  node is in your topo sort, so please remove it from the indegree
+		  
+		  for(auto it : adj[node]){
+		      indegree[it]--;
+		      if(indegree[it]==0)
+		        q.push(it);
+		  }
+		}
+		
+		if (cnt==V)
+		    return false;
+		return true;
 	}
 };
 

b8c937d230bd102ad78c2a2aa92b40537a301b5d
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2022-11-23 22:13:19 +0530

    Time: 28 ms (9.59%), Space: 13.9 MB (91.12%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    void dfs(int row, int col, vector<vector<int>>&ans,
+     vector<vector<int>>& image, int newColor, int delRow[], int delCol[],
+     int iniColor){
+        // color with new color
+        ans[row][col] = newColor;
+        int n = image.size();
+        int m = image[0].size();
+        
+        // there are exactly four neighbours
+        for(int i=0; i<4; i++){
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+        
+        
+            // check for valid coordinate 
+            // then cheack for same invalid colorand unvisited pixcel 
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
+                image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor){
+                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor);
+            }
+        }
+    }
+    
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // to get the initial color 
+        int iniColor = image[sr][sc];
+        vector<vector<int>> ans = image;
+        
+        // delta row and delta column for neighbours, to traverse a neighbours
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, +1, 0, -1}; 
+        
+        dfs(sr, sc, ans, image, color, delRow, delCol, iniColor);
+        
+        return ans;
+    }
+};

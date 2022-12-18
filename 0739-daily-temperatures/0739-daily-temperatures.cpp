4f2ae844b13aec4bed87999aca7d207effe6a81f
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2022-12-18 12:53:21 +0530

    Time: 228 ms (83.67%), Space: 98.6 MB (89.86%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -21,3 +21,24 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& arr) {
+//         int n = arr.size();
+//         vector<int> ans(n, 0);
+        
+//         for(int i = n - 1; i >= 0; i--) {
+//             int j = i + 1;
+//             while(j < n && arr[j] <= arr[i]) {
+//                 j++;
+//             }
+//             if(j != n) {
+//                 ans[i] = j - i;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };

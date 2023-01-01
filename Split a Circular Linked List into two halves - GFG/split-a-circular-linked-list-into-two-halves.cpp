af9608d7a9c68e0bbeb320df3f769d4e0f689471
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2023-01-01 18:09:46 +0530

    Added solution - LeetHub

---
+++ Split a Circular Linked List into two halves - GFG/split-a-circular-linked-list-into-two-halves.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+/* Program to split a circular linked list into two halves */
+
+
+#include<stdio.h> 
+#include<stdlib.h> 
+ 
+/* structure for a Node */
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+}; 
+
+void splitList(struct Node *head, struct Node **head1_ref,   struct Node **head2_ref);
+ struct Node* newNode(int key)
+{
+    struct Node *temp = new Node(key);
+    
+    return temp;
+}
+/* Function to split a list (starting with head) into two lists.
+   head1_ref and head2_ref are references to head Nodes of 
+    the two resultant linked lists */
+
+void printList(struct Node *head)
+{
+  struct Node *temp = head; 
+  if(head != NULL)
+  {
+    do {
+      printf("%d ", temp->data);
+      temp = temp->next;
+    } while(temp != head);
+    printf("\n");
+  }
+}
+
+ 
+/* Driver program to test above functions */
+int main()
+{
+  
+   int t,n,i,x;
+    scanf("%d",&t);
+   while(t--)
+   {
+	   struct Node *temp,*head = NULL;
+       struct Node *head1 = NULL;
+       struct Node *head2 = NULL;
+	   scanf("%d",&n);
+	   scanf("%d",&x);
+	   head=new Node(x);
+	   temp=head;
+	   for(i=0;i<n-1;i++){
+	   scanf("%d",&x);
+	   
+	   temp->next=new Node(x);
+	   
+	   temp=temp->next;
+	       
+	   }
+       
+       temp->next=head;
+  
+       splitList(head, &head1, &head2);
+  
+     // printf("\nFirst Circular Linked List");
+      printList(head1);  
+ 
+     // printf("\nSecond Circular Linked List");
+      printList(head2);  
+   
+   }
+  return 0;
+}
+
+// } Driver Code Ends
+
+
+void splitList(Node *head, Node **head1_ref, Node **head2_ref)
+{
+   if(head==NULL or head->next==NULL){
+       return;
+   }
+   // Step 1 : find middle point using slow and fast pointer
+   Node* slow=head;
+   Node* fast=head->next;
+   
+   while(fast!=head and fast->next!=head){
+       slow=slow->next;
+       fast=fast->next->next;
+   }
+   
+   
+   Node* afterMid=slow->next;
+   
+   //Step 2 : making 1st circular Linked list head to slow
+   *head1_ref=head;
+   slow->next=head;
+   
+   //Step 3 : making 2nd circular Linked list slow to last
+   *head2_ref=afterMid;
+   Node* temp=*head2_ref;
+   
+   while(temp->next!=head){
+      temp = temp->next;
+   }
+  temp->next = *head2_ref;
+   
+}

e33e9acc487da366810de27156ac8a2935b6aabf
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2023-02-15 19:14:56 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // an array to check if the node is visited or not 
+        int vis[V]= {0};
+        vis[0]=1;
+        
+        // creating a queue "q" and inserting the forst element
+        queue<int> q;
+        q.push(0);
+        
+        // a vector to store the bfs traversal 
+        vector<int> bfs;
+        
+        // treversing the graph
+        while(!q.empty()){
+            // removing cureent node from the queue and inserting in vector 
+            int node = q.front(); 
+            q.pop(); 
+            bfs.push_back(node); 
+            // traverse for all its neighbours 
+            for(auto it : adj[node]) {
+                // if the neighbour has previously not been visited, 
+                // store in queue and mark as visited 
+                if(!vis[it]) {
+                    vis[it] = 1; 
+                    q.push(it); 
+                }
+            }
+        }
+        return bfs;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

9fb052b752cc8832bf50b96d51c6f036b7bd45e6
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2023-02-23 13:42:38 +0530

    Time: 20 ms (50.62%), Space: 13.6 MB (77.85%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;
+        }
+        priority_queue<pair<int,int>> pq;  //Fisrt element stores frequency and second element value
+		//As we have not use any compare function max heap will sort according to first element of pair
+        unordered_map<int,int>::iterator itr;
+        for(itr=m.begin();itr!=m.end();itr++){
+            pq.push(make_pair(itr->second,itr->first));
+        }
+        vector<int> ans(k);
+        for(int i=0;i<k;i++){
+            ans[i]=pq.top().second;
+            pq.pop();
+        }
+        return ans;
+    }
+};

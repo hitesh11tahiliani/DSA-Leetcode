c96e9a834fa28ad8a320a48686f3e8e96e86d1b5
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2023-03-06 22:09:44 +0530

    Time: 38 ms (62.32%), Space: 14.6 MB (21.32%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -2,41 +2,43 @@ class Solution
 {
 public:
 	int kthSmallest(vector<vector<int>>& matrix, int k)
-	{
-		int n = matrix.size();
-		int le = matrix[0][0], ri = matrix[n - 1][n - 1];
-		int mid = 0;
-		while (le < ri)
-		{
-			mid = le + (ri-le)/2;
-			int num = 0;
-			for (int i = 0; i < n; i++)
-			{
-				int pos = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
-				num += pos;
-			}
-			if (num < k)
-			{
-				le = mid + 1;
-			}
-			else
-			{
-				ri = mid;
-			}
-		}
-		return le;
-    }
-//           int count =0;
-//           vector<int> ans;
+	// {
+	// 	int n = matrix.size();
+	// 	int le = matrix[0][0], ri = matrix[n - 1][n - 1];
+	// 	int mid = 0;
+	// 	while (le < ri)
+	// 	{
+	// 		mid = le + (ri-le)/2;
+	// 		int num = 0;
+	// 		for (int i = 0; i < n; i++)
+	// 		{
+	// 			int pos = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+	// 			num += pos;
+	// 		}
+	// 		if (num < k)
+	// 		{
+	// 			le = mid + 1;
+	// 		}
+	// 		else
+	// 		{
+	// 			ri = mid;
+	// 		}
+	// 	}
+	// 	return le;
+	// }
+    {
+          int count =0;
+        int n = matrix.size();
+          vector<int> ans;
 
-//           for(int i=0;i<n;i++)
-//             {
-//                 for(int j=0;j<n;j++)
-//                 {
-//                     ans.push_back(matrix[i][j]);
-//                 }
-//             }
-//             sort(ans.begin(), ans.end());
-//             return ans[k-1];
-//             }
+          for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+            sort(ans.begin(), ans.end());
+            return ans[k-1];
+            }
 };

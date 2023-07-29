d24ed567102170053745273e7adc312de5e10b83
Hitesh Tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2023-07-29 19:05:06 +0530

    Time: 3 ms (73.75%), Space: 7 MB (89.34%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        
-        // helper variables
-        int res=0;
-        int i=0;
-        int sign=1;
-		
-        while(i<s.size()&&s[i]==' ')i++;  //ignore leading white space
-        
-        if(s[i]=='-'||s[i]=='+')          //check if number positve or negative
-        {
-            sign=s[i]=='-'?-1:1;
+        int i = 0, n = s.size();
+
+        while(s[i] == ' '){
             i++;
         }
-        // now iterate across digits if any
-		// should only be in range 0-9
-        while(i<s.length()&&(s[i]>='0'&&s[i]<='9'))  //traverse string till nondigit not found or string ends
-        {
-            int digit=(s[i]-'0')*sign;
-            if(sign==1 && (res>INT_MAX/10 || (res==INT_MAX/10 && digit>INT_MAX%10))) return INT_MAX; //check for overflow
-            if(sign==-1 &&(res<INT_MIN/10 || (res==INT_MIN/10 && digit<INT_MIN%10))) return INT_MIN; //check for underflow
-            
-            res=res*10+digit; // update res
+
+        int positive = 0, negative = 0; 
+
+        if(s[i] == '+'){
+            positive++; 
             i++;
         }
-    
-    return res;
+
+        if(s[i] == '-'){
+            negative++;
+            i++;
+        }
+
+        double ans = 0; 
+
+        while(i<n and s[i] >= '0' and s[i] <= '9'){
+            ans = ans*10 + (s[i] - '0'); 
+            i++;
+        }
+
+        if(negative > 0)
+            ans = -ans;
+
+        if(positive > 0 and negative > 0)
+            return 0;
+
+        if(ans > INT_MAX)
+            ans = INT_MAX;
+
+        if(ans < INT_MIN)
+            ans = INT_MIN;
+
+        return ans;
     }
 };
